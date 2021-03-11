8d0fde0d9ae7ec4735bc444f01a579a5d09d5050
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-11 17:16:23 +0100

    Crea Estado del Valor de la Búsqueda && Hace Función en onSubmit && Construye el Componente de Error

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Error from './Error';
 
 function Formulario() {
+    const [valorInput, setValorInput] = useState('');
+    const [error, setError] = useState(false);
+
+    const buscarImagenes = (e) => {
+        e.preventDefault();
+
+        // Validar
+        if (valorInput.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
+
+        // Enviar el value de la búsqueda al componente principal
+    };
+
     return (
-        <form>
+        <form onSubmit={buscarImagenes}>
             <div className='row'>
                 <div className='form-group col-md-8'>
                     <input
                         type='text'
                         className='form-control form-control-lg'
                         placeholder='Busca una imagen, ejemplo: Gato'
+                        onChange={(e) => setValorInput(e.target.value)}
                     />
                 </div>
                 <div className='form-group col-md-4'>
@@ -19,6 +37,10 @@ function Formulario() {
                     />
                 </div>
             </div>
+
+            {error ? (
+                <Error mensaje='Rellene la búsqueda correctamente' />
+            ) : null}
         </form>
     );
 }
