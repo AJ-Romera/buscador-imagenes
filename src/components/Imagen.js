c748ceaf780503d49e0cc9487ded8e5eab21b0ab
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-12 16:48:09 +0100

    FIX: Coloca Misma Altura para Cada Card

--- src/components/Imagen.js
+++ src/components/Imagen.js
@@ -14,7 +14,7 @@ function Imagen({ imagen }) {
 
     return (
         <div className='col-12 col-sm-6 col-md-4 col-lg-3 mb-4'>
-            <div className='card'>
+            <div className='card' style={{ height: '35rem' }}>
                 <img src={previewURL} alt={tags} className='card-img-top' />
 
                 <div className='card-body'>
